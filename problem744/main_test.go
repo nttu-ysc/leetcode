aab167ec6c02a5b315d220a62877730e34039cef
Shun <a7932677@gmail.com>
2023-06-09 10:29:46 +0800

    feat: + problem 744

---
+++ problem744/main_test.go
@@ -0,0 +1,33 @@
+package problem744
+
+import "testing"
+
+func TestNextGreatestLetter(t *testing.T) {
+	tests := []struct {
+		letters []byte
+		target  byte
+		want    byte
+	}{
+		{
+			letters: []byte{'c', 'f', 'j'},
+			target:  'a',
+			want:    'c',
+		},
+		{
+			letters: []byte{'c', 'f', 'j'},
+			target:  'c',
+			want:    'f',
+		},
+		{
+			letters: []byte{'x', 'x', 'y', 'y'},
+			target:  'z',
+			want:    'z',
+		},
+	}
+
+	for _, test := range tests {
+		if got := nextGreatestLetter(test.letters, test.target); got != test.want {
+			t.Errorf("Test failed. The input letters=%v target=%v  is excepted to %v but return %v", test.letters, test.target, test.want, got)
+		}
+	}
+}
